1652db389d2f4c305e1f4b4ff07c0615592262f2
Axenov97 <dimka180497@mail.ru>
2021-12-23 17:44:26 +0300

    sendmail

--- controllers/sendmailController.js
+++ controllers/sendmailController.js
@@ -5,7 +5,7 @@ const mailer = require("nodemailer")
 class SendmailController {
     async send(req, res, next) {
         try{
-            let { name, email, phone, text } = req.body
+            let { name, email, phone, text, button } = req.body
             const transporter = mailer.createTransport({
                 service: "Yandex",
                 auth: {
@@ -16,11 +16,14 @@ class SendmailController {
 
             const mailOptions = {
                 from: 'web.baltkomlekt@yandex.ru', //отправитель
-                to: "dimka180497@mail.ru, web.baltkomlekt@yandex.ru", //получатели
+                to: "web.baltkomlekt@yandex.ru", //получатели
                 subject: "Заявка с сайта", //заголовок
                 // text: text
-                html: `<div style="border:1px solid black;padding:20px;
-                            font-family:sans-serif;line-height:2;font-size:20px; ">
+                html: `<div style="border:1px solid black;padding:10px;
+                            font-family:sans-serif;line-height:2;font-size:16px;">
+                            <h1 style="font-size: 20px;font-weight: bold;">
+                                Заявка по кнопке ${button}
+                            </h1>
                             <p>Имя: ${name}</p>
                             <p>Email: ${email}</p>
                             <p>Телефон: ${phone}</p>
