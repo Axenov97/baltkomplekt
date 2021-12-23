9caf05965d605488b103183051c78acbdda94b84
Axenov97 <dimka180497@mail.ru>
2021-12-23 13:47:35 +0300

    sendMail

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ const faqRouter = require('./faqRouter')
 const casesRouter = require('./casesRouter')
 const materialsRouter = require('./materialsRouter')
 const newsRouter = require('./newsRouter')
-// const sendmailRouter = require('./sendmailRouter')
+const sendmailRouter = require('./sendmailRouter')
 
 router.use('/user', userRouter)
 router.use('/news', newsRouter)
 router.use('/faq', faqRouter)
 router.use('/case', casesRouter)
 router.use('/materials', materialsRouter)
-// router.use('/sendmail', sendmailRouter)
+router.use('/sendmail', sendmailRouter)
 
 
 module.exports = router
